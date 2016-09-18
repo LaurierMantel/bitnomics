c9754b16b2faed25ec5f9d4f3a621941318bd828
Nolan Dey <ndey96@gmail.com>
2016-09-18 05:54:13 -0400

    6am

--- vis/src/roach.js
+++ vis/src/roach.js
@@ -1,27 +1,31 @@
 var pg = require('pg');
 
 var config = {
-  user: 'root', //env var: PGUSER
-  database: 'cockdb69', //env var: PGDATABASE
-  password: 'Hack123', //env var: PGPASSWORD
-  host: 'localhost', // Server hosting the postgres database
-  port: 26257, //env var: PGPORT
+  user: 'root', //env var: PGUSER 
+  database: 'cockdb69', //env var: PGDATABASE 
+  password: 'Hack123', //env var: PGPASSWORD 
+  port: 26257, //env var: PGPORT 
+  host: 'localhost',
+  max: 10, // max number of clients in the pool 
+  idleTimeoutMillis: 30000 // how long a client is allowed to remain idle before being closed 
 };
 
-
+var pool = new pg.Pool(config);
+pool.on('error', function (err, client) {
+  console.error('idle client error', err.message, err.stack)
+});
 exports.getTransactions = function(cb) {
-	var client = new pg.Client(config);
-	client.connect(function(err) {
-	    if (err) throw err;
-	    client.query('SELECT * from test_transactions limit 100', function(error, result) {
+	pool.connect(function(err, client, done) {
+	  if(err) {
+	    return console.error('error fetching client from pool', err);
+	  }
+	 	client.query('SELECT * from test_transactions limit 100', function(error, result) {
 	    	if (error) {
 	      	    return cb('error running query');
 	      	}
 			console.log('hit em with the callback');
 	      	cb(result.rows);
-		    client.end(function (err) {
-		    	if (err) throw err;
-		    });
-	    });
+    	});
 	});
 };
+
